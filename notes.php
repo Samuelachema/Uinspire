093e5c7c295b3112fd3f745c9181698e77dfe203
@SamuelAchema <achemaku@gmail.com>
2018-07-22 20:31:10 +0100

    Initial commit

---
+++ notes.php
@@ -0,0 +1,32 @@
+<?php
+
+/* 
+* ProjectName:	Uinspire
+* Description: 	This application is designed to help LightHouse Leaders (Church Home Cell leaders) of Inspiration Life 
+*				Community Church Submit their meeting reports to supervisors and keep records of their previous reports.
+* Author:		Samuel Achema(1stSam)
+* Version:		1.0
+* Date:			21-07-2018
+*/
+
+/*	
+	***Features***
+	1. LightHouse Leaders should be able to add members
+	2. Leaders should be able to fill reports, preview and submit it
+	3. Supervisors should be able to view reports, revert for correction, collate and score reports
+	
+	*** TO DO LIST***
+	1. Design UI											1
+	2. Design Database Model								1
+	3. Search and download appropriate template				0
+	4. Develop web UI										0
+	5. Develop database and populate data					0
+	6. Alpha testing										0
+	7. Debug app											0
+	8. Beta testing											0
+	9. Debug app											0
+	10. Offline first implementation						0
+	11.Market and present application/ mobile app			0
+	12.Start planning for version 2							0	
+
+*/
