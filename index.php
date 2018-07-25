ea906bf228df4ba2e3e9fb2eb57123b6effe8b15
@SamuelAchema <achemaku@gmail.com>
2018-07-25 16:55:24 +0100

    redirects to admin index
    redirects to admin index

--- index.php
+++ index.php
@@ -1,25 +1,3 @@
 <?php
 
-//This is my very first comment
-
-//echo "I have no idea what to do next";
-
-//Practice some OOP php
-class User{
- public $name = "Brad Traversy";
- 
- function myFunction(){
-	 echo $this->name;
- }
- function __construct(){
-	 echo "This is a constructor. It is called automatically when an object is instantiated.";
- }
- function __destruct(){
-	 echo "This is a destructor. It is called automatically when an object is instantiated.";
- }
-}
-
-$user = new User;
-
-//$user->myFunction();
-//echo $user->name;
+header("Location:admin/");
