6ba6187949f05ab802a9d91089cfba72ccd892f4
@SamuelAchema <achemaku@gmail.com>
2018-07-27 21:31:06 +0100

    added function folder
    added function folder

---
+++ admin/includes/footer.php
@@ -0,0 +1,72 @@
+		 <!-- Footer -->
+            <footer id="footer">
+                <div class="pattern-overlay">
+                    
+                    <!-- Footer Bottom -->
+                    <div class="footer-bottom">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-6 col-md-6 col-xs-12 col-sm-6 ">
+                                    <p class="credits">&copy; Copyright 2014 by <a href="#">FIFOLAB</a>. All Rights Reserved. </p>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-xs-12 col-sm-6 ">
+                                    <ul class="social social-icons-footer-bottom">
+                                        <li class="facebook"><a href="#" data-toggle="tooltip" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                                        <li class="twitter"><a href="#" data-toggle="tooltip" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                                        <li class="dribbble"><a href="#" data-toggle="tooltip" title="Dribble"><i class="fa fa-dribbble"></i></a></li>
+                                        <li class="linkedin"><a href="#" data-toggle="tooltip" title="LinkedIn"><i class="fa fa-linkedin"></i></a></li>
+                                        <li class="rss"><a href="#" data-toggle="tooltip" title="Rss"><i class="fa fa-rss"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /Footer Bottom --> 
+                </div>
+            </footer>
+			<!-- /Footer -->
+            
+			<!-- Scroll To Top --> 
+            <a href="#" class="scrollup"><i class="fa fa-angle-up"></i></a>
+        </div>
+        <!-- /Wrap -->
+        <!-- Style Switcher -->
+        
+        <!-- /Style Switcher -->
+        <!-- The Scripts -->
+		<!-- The Scripts -->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/jquery-migrate-1.0.0.js"></script>
+        <script src="js/jquery-ui.js"></script>
+        <script src="js/bootstrap.js"></script>
+        <script src="js/revolution-slider/js/jquery.themepunch.plugins.min.js"></script> 
+        <script src="js/revolution-slider/js/jquery.themepunch.revolution.min.js"></script>
+        <script src="js/jquery.parallax.js"></script>
+        <script src="js/jquery.wait.js"></script>
+        <script src="js/fappear.js"></script> 
+        <script src="js/modernizr-2.6.2.min.js"></script>
+        <script src="js/jquery.bxslider.min.js"></script>
+        <script src="js/jquery.prettyPhoto.js"></script>
+        <script src="js/superfish.js"></script>
+        <script src="js/tweetMachine.js"></script>
+        <script src="js/tytabs.js"></script>
+        <script src="js/jquery.gmap.min.js"></script>
+        <script src="js/jquery.sticky.js"></script>
+        <script src="js/jquery.countTo.js"></script>
+        <script src="js/jflickrfeed.js"></script>
+        <script src="js/imagesloaded.pkgd.min.js"></script>
+        <script src="js/waypoints.min.js"></script>
+        <script src="js/wow.js"></script>
+        <script src="js/jquery.fitvids.js"></script>
+        <script src="js/spectrum.js"></script>
+        <script src="js/switcher.js"></script>
+        <script src="js/custom.js"></script>
+    </body>
+
+</html>
+<?php
+	// 5. Close connection
+	if(isset($connection)){
+	mysql_close($connection);
+	}
+?>
---
+++ admin/includes/header.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<!--[if IE 8]>          <html class="ie ie8"> <![endif]-->
+<!--[if IE 9]>          <html class="ie ie9"> <![endif]-->
+<!--[if gt IE 9]><!-->  
+<html> 
+	<!--<![endif]-->
+    
+
+<head>
+        <meta charset="utf-8">
+        <title>HelpDesk | Customer Complaints Resolution</title>
+        <meta name="HelpDesk" content="Customer Complaints Resolution">
+        <meta name="author" content="SynchronyLtd">
+        <!-- Mobile Metas -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Google Fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Raleway:100,200,300,700,800,900' rel='stylesheet' type='text/css'>
+        <!-- Library CSS -->
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.css">
+        <link rel="stylesheet" href="css/fonts/font-awesome/css/font-awesome.css">
+        <link rel="stylesheet" href="css/animations.css" media="screen">
+        <link rel="stylesheet" href="css/superfish.css" media="screen">
+        <link rel="stylesheet" href="css/revolution-slider/css/settings.css" media="screen">
+        <link rel="stylesheet" href="css/revolution-slider/css/extralayers.css" media="screen">
+        <link rel="stylesheet" href="css/prettyPhoto.css" media="screen">
+        <!-- Theme CSS -->
+        <link rel="stylesheet" href="css/style.css">
+        <!-- Skin -->
+        <link rel="stylesheet" href="css/colors/green.css" class="colors">
+        <!-- Responsive CSS -->
+        <link rel="stylesheet" href="css/theme-responsive.css">
+        <!-- Switcher CSS -->
+        <link href="css/switcher.css" rel="stylesheet">
+        <link href="css/spectrum.css" rel="stylesheet">
+        <!-- Favicons -->
+        <link rel="shortcut icon" href="img/ico/favicon.ico">
+        <link rel="apple-touch-icon" href="img/ico/apple-touch-icon.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="img/ico/apple-touch-icon-72.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="img/ico/apple-touch-icon-114.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="img/ico/apple-touch-icon-144.png">
+        <!-- The HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+        <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <script src="js/respond.min.js"></script>
+        <![endif]-->
+        <!--[if IE]>
+        <link rel="stylesheet" href="css/ie.css">
+        <![endif]-->
+    </head>
+	
+	<body class="page">
+        <div class="page-mask">
+            <div class="page-loader">
+                <div class="spinner"></div>
+                Loading...
+            </div>
+        </div>
+		<!-- Wrap -->
+        <div class="wrap">
+            <!-- Header -->
+            <header id="header">
+                <!-- Header Top Bar -->
+                <div class="top-bar">
+                    <div class="slidedown collapse">
+                        <div class="container">
+                            <div class="pull-left">
+                                <ul class="social pull-left">
+                                    <li class="facebook"><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                    <li class="twitter"><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                    <li class="dribbble"><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                                    <li class="linkedin"><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                    <li class="rss"><a href="#"><i class="fa fa-rss"></i></a></li>
+                                </ul>
+                            </div>
+                            <div class="phone-login pull-right">
+                                <a><i class="fa fa-phone"></i> Call Us : +880 111-111-111</a>
+                                <a href="login.php" data-toggle="#" data-target="#"><i class="fa fa-sign-in"></i> Login</a>
+                                <a href="#" data-toggle="modal" data-target="#registrationModal"><i class="fa fa-edit"></i> Registration</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+				<!-- /Header Top Bar -->
+				<!-- Main Header -->
+                <div class="main-header">
+                    <div class="container">
+                        <!-- TopNav -->
+                        <div class="topnav navbar-header">
+                            <a class="navbar-toggle down-button" data-toggle="collapse" data-target=".slidedown">
+                            <i class="fa fa-angle-down icon-current"></i>
+                            </a> 
+                        </div>
+                        <!-- /TopNav-->
+                        <!-- Logo -->
+                        <div class="logo pull-left">
+                            <h1>
+                                <a href="index-2.html">
+                                <img class="logo-color" src="img/logos/logo_green.png" alt="gallaxy" width="160" height="60">
+                                </a>
+                            </h1>
+                        </div>
+                        <!-- /Logo -->
+                        <!-- Mobile Menu -->
+                        <div class="mobile navbar-header">
+                            <a class="navbar-toggle" data-toggle="collapse" href=".html">
+                            <i class="fa fa-bars fa-2x"></i>
+                            </a> 
+                        </div>
+                        <!-- /Mobile Menu -->
+                        <!-- Menu Start -->
+                        
